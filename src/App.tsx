4ab90274033c06232c0f21c56fd8ca6111a019a5
marcelo <sancheloz@gmail.com>
2022-10-05 01:03:55 -0400

    Adding API request for movies and series

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Box, Grommet, Text } from 'grommet';
 import { theme } from './theme';
+import axios from 'axios';
+import { AppContext } from './context/AppContext';
 
 function App() {
+  const { heroList } = useContext(AppContext);
+
+  const [moviesList, setMoviesList] = useState([]);
+  const [seriesList, setSeriesList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://mcuapi.herokuapp.com/api/v1/movies')
+      .then(function ({ data }) {
+        console.log('movies: ', data.data);
+        setMoviesList(data.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    axios
+      .get('https://mcuapi.herokuapp.com/api/v1/tvshows')
+      .then(function ({ data }) {
+        console.log('series: ', data.data);
+        setSeriesList(data.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <Grommet theme={theme} full>
       <Box fill direction="column">
