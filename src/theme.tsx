46932bc374d059d4f249332d5ab0be70bec96d06
marcelo <sancheloz@gmail.com>
2022-10-04 20:02:34 -0400

    Cleaning project, and adding Grommet and styed-components libraries

---
+++ src/theme.tsx
@@ -0,0 +1,21 @@
+import { grommet } from 'grommet';
+import { deepMerge } from 'grommet/utils';
+
+export const theme = deepMerge(grommet, {
+  global: {
+    colors: {
+      brand: '#2D2D2D',
+      white: '#FFFFFF',
+      black: '#000000',
+      secBrand: '#666666',
+      background: '#FFFFFF',
+      secBg: '#F8F8F8'
+    },
+    font: {
+      family: 'Roboto',
+      style: 'normal',
+      size: '15px',
+      height: '17px'
+    }
+  }
+});
